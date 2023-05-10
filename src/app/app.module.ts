ca7361165f8807aa3d5fbeedb9b7ead674b6110a
Ali Ali <41684444+Ali-Ali1@users.noreply.github.com>
2023-05-09 22:45:00 -0400

    created shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire/compat'
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
 
 import { AppComponent } from './app.component';
+import { enviroment } from 'src/environments/environment';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(enviroment.firebaseconfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+    production: false,
+    firebaseconfig : {
+        apiKey: "AIzaSyCmFVDhj0AR922i7AlY0f-1o6ewT9rVnA0",
+        authDomain: "todoapp-f9dbb.firebaseapp.com",
+        projectId: "todoapp-f9dbb",
+        storageBucket: "todoapp-f9dbb.appspot.com",
+        messagingSenderId: "188087664899",
+        appId: "1:188087664899:web:9ab6615fc6cd4bcc226936"
+      }
+}
+
